e4f6ee2d4197e2de78bda5215d2ade7600902e1e
Dongjin <dongjin@plask.ai>
2023-10-04 21:30:29 +0900

    add job

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
           children: <Widget>[
-            CircleAvatar(
+            const CircleAvatar(
               radius: 50,
               backgroundImage: NetworkImage(
                   'https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp'),
             ),
-            Text('IU',
+            const Text('IU',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                   fontSize: 40,
-                ))
+                )),
+            Text('Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 3.5,
+                    fontWeight: FontWeight.bold))
           ],
         )),
       ),
